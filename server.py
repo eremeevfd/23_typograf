1e96a436203f7910b84f16aaa2b9433bfc6423ec
eremeevfd <dartfedor@mail.ru>
2017-02-11 18:37:42 +0300

    Function output returns to lower textarea.

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from typograf import typograf
 
 app = Flask(__name__)
 
 @app.route('/')
 def form():
     return render_template('form.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = typograf(text)
+    return render_template('form.html', content=processed_text)
+
 
 if __name__ == "__main__":
     app.run()
